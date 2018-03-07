e5f68935742ee0164b76dc677f09ac10b8b0385e
Awlter <wxfwalter@gmail.com>
2018-03-07 23:45:54 +0800

    cache pvs every 1s if the front page got visited

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,11 +1,16 @@
 class PageController < ApplicationController
-  before_action :set_and_add_uvs, :set_and_add_pvs
+  before_action :set_page_title, :set_and_add_uvs, :set_and_add_pvs, only: [:front]
+  after_action :save_pvs
 
   def front
 
   end
 
   private
+
+  def set_page_title
+    @page_title = self.class.to_s.gsub('Controller', '')
+  end
 
   def set_and_add_uvs
     ip = request.remote_ip
@@ -15,9 +20,42 @@ class PageController < ApplicationController
   end
 
   def set_and_add_pvs
-    page_title = self.class.to_s.gsub('Controller', '')
-    @pvs = Rails.cache.read(page_title) || Rails.cache.write(page_title, PagesViewCount.find_by(page_title: page_title).counter)
+    unless !!read_pvs
+      Rails.cache.write(@page_title, PagesViewCount.find_by(page_title: @page_title).counter)
+    end
 
-    Rails.cache.write(page_title, @pvs + 1)
+    @pvs = read_pvs
+
+    Rails.cache.write(@page_title, @pvs + 1)
+  end
+
+  def read_pvs
+    Rails.cache.read(@page_title)
+  end
+
+  def save_pvs
+    if more_than_1_secs?
+      set_last_time
+      PagesViewCount.find_by(page_title: @page_title).update(counter: @pvs)
+    end
+  end
+
+  def more_than_1_secs?
+    Time.now - last_time > 1
+  end
+
+  def last_time
+    unless !!read_last_time
+      set_last_time
+    end
+    read_last_time
+  end
+
+  def read_last_time
+    Rails.cache.read('last_time')
+  end
+
+  def set_last_time
+    Rails.cache.write('last_time', Time.now)
   end
 end
